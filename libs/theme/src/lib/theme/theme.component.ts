d9912a23c0dc6b944f560cb5fb90270a54cc2516
Tony <tonyxavier@live.com>
2023-12-27 01:04:18 -0500

    feat: theming

---
+++ libs/theme/src/lib/theme/theme.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'hub-theme',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './theme.component.html',
+  styleUrl: './theme.component.css',
+})
+export class ThemeComponent {}
